0d843550ed708dcdf8681e41d8b244e93f0f0f32
Sam Manzer <samuelmanzer@users.noreply.github.com>
2015-07-06 18:24:10 -0700

    Display now right but peaks out of phase

--- wavepacket.py
+++ wavepacket.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import matplotlib
+matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -22,33 +24,30 @@ mag = np.trapz(np.multiply(np.absolute(psi_p_initial),np.absolute(psi_p_initial)
 
 plt.ion()
 # Position-space plot
-fig0 = plt.figure()
-ax0 = fig0.add_subplot(131)
+fig = plt.figure()
+ax0 = plt.subplot2grid((2,3),(0,0))
 ax0.set_xlim([-xdist,xdist])
 psi_x_line, = ax0.plot(xpoints,np.square(psi_x),color="blue")
 
 # Momentum-space plot
-ax1 = fig0.add_subplot(132)
+ax1 = plt.subplot2grid((2,3),(0,1))
 ax1.set_xlim([-10,10])
 psi_p_line, = ax1.plot(2*np.pi*freqs,(1/mag)*np.square(np.absolute(psi_p_initial)),color="red")
 
 # Analytic momentum space plot
-ax2 = fig0.add_subplot(133)
+ax2 = plt.subplot2grid((2,3),(0,2))
 psi_p_analytic_line, = ax2.plot(ppoints,np.square(np.absolute(psi_p_analytic)),color="green")
-psi_p_analytic_line.set_ydata(np.square(np.absolute(psi_p_analytic)))
-
 
 # Fourier representation periodicity plot 
 # Six periodic instances of the Gaussian
-ax3 = fig0.add_subplot(211)
+ax3 = plt.subplot2grid((2,3),(1,0),colspan=3)
 periodic_interval =  np.linspace(-6*xdist,6*xdist,6*npoints,True)
 all_waves_at_all_points = np.exp(2*np.pi*1j*np.outer(freqs,periodic_interval))
 psi_x_periodic = 1.0/(len(psi_p_initial))*np.dot(psi_p_initial,all_waves_at_all_points)
 psi_x_periodic_line, = ax3.plot(periodic_interval,np.square(np.absolute(psi_x_periodic)),color="purple")
 
-#psi_x_periodic = x_norm* 
-#fig0.tight_layout()
-fig0.subplots_adjust(wspace=0.1,hspace=0.15)
+plt.tight_layout()
+
 # Propagate - set particle mass magnitue equal to 2*hbar for simplicity
 for time_step in np.arange(0,1000,1):
     psi_p = np.multiply(np.exp(-1j*np.square(freqs)*time_step),psi_p_initial)
@@ -61,5 +60,5 @@ for time_step in np.arange(0,1000,1):
     mag = np.trapz(np.multiply(np.absolute(psi_p),np.absolute(psi_p)),2*np.pi*freqs)
     psi_p_line.set_ydata((1/mag)*np.square(np.absolute(psi_p)))
 
-    fig0.canvas.draw()
+    plt.draw()
     time.sleep(0.01)
